8f7d777708d3abe44681f4e7c90ec60200476654
Monaeid2001 <104376815+Monaeid2001@users.noreply.github.com>
2023-10-25 14:44:51 +0300

    Add files via upload

---
+++ lab5-1-1/main.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+int swap(int *ptr1,int *ptr);
+int main() {
+int x=0,y=0;
+int *ptr1=&x;
+int *ptr2=&y;
+printf("enter two numbers");
+scanf("%d",&x);
+scanf("%d",&y);
+printf("the two number before swapping %d %d\n",x,y);
+swap(&x,&y);
+printf("the two number before swapping %d %d",x,y);
+return;
+}
+int swap(int *ptr1,int *ptr2){
+  *ptr1 = *ptr1* *ptr2;
+ *ptr2= *ptr1/ *ptr2;
+  *ptr1= *ptr1/ *ptr2;
+
+}
